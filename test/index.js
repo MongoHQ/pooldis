9d2bb18354b6e5e75760ec8de8855de2cd182536
Jerome Gravel-Niquet <jeromegn@gmail.com>
2013-08-09 14:24:58 -0400

    lots of fixes

--- test/index.js
+++ test/index.js
@@ -1,12 +1,56 @@
 var Pooldis = require("../lib"),
-    assert = require("assert"),
+    assert = require("chai").assert,
     sinon = require("sinon");
 
 describe("Pooldis", function(){
   describe("instantiation", function(){
-    it("without arguments uses default settings", function(){
-      var redis = new Pooldis
-      assert(redis instanceof Pooldis)
+    var redis;
+    before(function(){
+      redis = new Pooldis();
+    });
+    it("instantiates a client without a host", function(){
+      assert.equal(redis.connectArgs[1], null);
+    })
+    it("instantiates a client without a port", function(){
+      assert.equal(redis.connectArgs[0], null);
+    })
+    describe("with a valid redis URL", function(){
+      describe("without a port", function(){
+        var redis;
+        before(function(){
+          redis = new Pooldis("redis://127.0.0.1");
+        });
+        it("instantiates a client with the right host", function(){
+          assert.equal(redis.connectArgs[1], "127.0.0.1");
+        })
+        it("instantiates a client with the right port", function(){
+          assert.equal(redis.connectArgs[0], 6379);
+        })
+      })
+      describe("with a port", function(){
+        var redis;
+        before(function(){
+          redis = new Pooldis("redis://127.0.0.1:6379");
+        });
+        it("instantiates a client with the right host", function(){
+          assert.equal(redis.connectArgs[1], "127.0.0.1");
+        })
+        it("instantiates a client with the right port", function(){
+          assert.equal(redis.connectArgs[0], 6379);
+        })
+      })
+    });
+    describe("with an options object", function(){
+      var redis;
+      before(function(){
+        redis = new Pooldis({hostname: "127.0.0.1", port: 6379});
+      });
+      it("instantiates a client with the right host", function(){
+        assert.equal(redis.connectArgs[1], "127.0.0.1");
+      })
+      it("instantiates a client with the right port", function(){
+        assert.equal(redis.connectArgs[0], 6379);
+      })
     });
   });
 });
